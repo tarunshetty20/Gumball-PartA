995661f99d556479d49c34bd44059a23c05dfba8
tarunshetty20 <31361726+tarunshetty20@users.noreply.github.com>
2018-06-19 06:53:39 +0530

    Add files via upload

--- Gumaball(assignment1)-partA/src/Main.java
+++ Gumaball(assignment1)-partA/src/Main.java
@@ -1,30 +1,31 @@
 public class Main {
 
     public static void main(String[] args) {
-//        GumballMachine mach1 = new GumballMachine(25);
-//        System.out.println("This is the first machine");
-//        mach1.insertQuarter(25);
-//        mach1.turnCrank();
-//        System.out.println("///////////////////////////");
-//
-//        System.out.println("This is the second machine");
-//        GumballMachine gumballMachine = new GumballMachine(50);
-//        gumballMachine.checkmachine2( 25);
-//        gumballMachine.checkmachine2( 25);
-//        gumballMachine.turnCrank();
         System.out.println("//////////////////////////////");
-        GumballMachine gumball = new GumballMachine(100,25,0);
-//        gumball.insertQuarter(25);
-//        gumball.insertQuarter(15);
-//        gumball.turnCrank();
-//        gumball.insertQuarter(25);
-//        gumball.insertQuarter(25);
-//        gumball.turnCrank();
+
+        ///// Pass parameters (number_of_balls, 25, 0) for the first gumball machine
+        ///// Pass parameters (number_of_balls, 50, 0) for the second gumball machine
+        ///// Pass parameters (number_of_balls, 50, 1) for the third gumball machine
+
+        GumballMachine gumball = new GumballMachine(100,50,1);
+
+        //Pass parameters and check for the third machine
+
         gumball.insertQuarter(25);
         gumball.insertQuarter(25);
         gumball.turnCrank();
         gumball.insertQuarter(25);
+        gumball.insertQuarter(25);
         gumball.turnCrank();
+
+        //Pass parameters and check for the first machine
+
+//        gumball.insertQuarter(25);
+//        gumball.turnCrank();
+//        gumball.insertQuarter(25);
+//        gumball.turnCrank();
+
+        // Pass parameters and check for second machine
 //        gumball.insertQuarter(25);
 //        gumball.insertQuarter(25);
 //        gumball.turnCrank();
